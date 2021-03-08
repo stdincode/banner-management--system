d9e46548efcf7eea493e6593a8efe7fb647d8a39
Rail <stdin.code@gmail.com>
2021-03-08 11:40:22 +0300

    added ad-place module

---
+++ modules/controllers/AdPlaceController.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace app\modules\controllers;
+
+/**
+ * Tag controller for the `modules` module
+ */
+class AdPlace extends BaseApiController
+{
+    public $modelClass = 'app\modules\models\AdPlaces';
+}
